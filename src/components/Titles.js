45f0363ebedda1e9a2b7420c2095e666639bedb8
Kelvin Almonte <KelvinAlmonte@live.com>
2019-08-26 13:01:38 -0400

    Calories rounded up. Style changes.

---
+++ src/components/Titles.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Titles = () => {
+  return (
+    <div className="title-box">
+      <h1>Recipe Pal</h1>
+      <h4>Try something new. Find the recipe for you.</h4>
+    </div>
+  );
+};
+
+export default Titles;
